13d2ccbef8f21315350fc213b987a49866fd77ac
Samuel Lee <lee.sam1492@gmail.com>
2023-03-28 13:55:20 -0600

    installed and configured tailwind. Working on home page

--- src/pages/homePage/HomePage.tsx
+++ src/pages/homePage/HomePage.tsx
@@ -1,14 +1,15 @@
-import themes from "../../styles/themes.module.scss";
+// import themes from "../../styles/themes.module.scss";
 import { router } from "../../router";
 import { Button } from "../../shared/Button";
 
 const HomePage: React.FC = () => {
   return (
-    <div style={{ color: themes.primaryBlue }}>
-      <h1>Choose Your Game!</h1>
-      <div>
+    <div className="p-6">
+      <h1 className="text-3xl font-bold text-center mb-4">Choose Your Game!</h1>
+      <div className="flex flex-col gap-8">
         <Button
           type={"button"}
+          variant={"bigButton"}
           text={"Memory Game"}
           onClick={() => router.navigate("/memory-game").catch(console.error)}
         />
